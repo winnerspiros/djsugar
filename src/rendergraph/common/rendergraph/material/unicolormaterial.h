04adb46b285b317332ee5648d8a296837d8dcdd5
winnerspiros <winnerspiros@users.noreply.github.com>
2026-06-15 22:45:25 +0000

    fix: unguard createShader() in unicolormaterial.h

--- src/rendergraph/common/rendergraph/material/unicolormaterial.h
+++ src/rendergraph/common/rendergraph/material/unicolormaterial.h
@@ -15,9 +15,7 @@ class rendergraph::UniColorMaterial : public rendergraph::Material {
 
     MaterialType* type() const override;
 
-#ifndef MIXXX_USE_LLGL
     std::unique_ptr<MaterialShader> createShader() const override;
-#endif
 
 #if defined(MIXXX_USE_LLGL) && !defined(RENDERGRAPH_SG)
     std::unique_ptr<LLGLMaterialShader> createLLGLShader() const override;
